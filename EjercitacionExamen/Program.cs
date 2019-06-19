dc6725ff9e15e036e4dd6a3c1b00fba573c4718f
MrCSG <50384291+MrCSG@users.noreply.github.com>
2019-06-19 10:58:11 -0300

    Ejercicio11 agregado (optimizar, en especial ejercicio10)

--- EjercitacionExamen/Program.cs
+++ EjercitacionExamen/Program.cs
@@ -8,10 +8,12 @@ namespace EjercitacionExamen
         {
             //Ejercicio1.Example();
             //Console.WriteLine( Ejercicio2.IsPalindrome( Console.ReadLine()));
-            //int[] A = new int[]{1, 3, 6, 4, 1, 2};
-            //int[] A = new int[]{1, 2, 3};
-            //int[] A = new int[]{-1, -3};
-            //Console.WriteLine(Ejercicio10.Solution(A));
+            /* int[] A = new int[]{1, 3, 6, 4, 1, 2};
+            int[] A = new int[]{1, 2, 3};
+            int[] A = new int[]{-1, -3};
+            Console.WriteLine(Ejercicio10.Solution(A)); */
+            /* int[] A = new int[]{0,1,0,1,1};
+            Console.WriteLine(Ejercicio11.Solution(A)); */
         }
     }
 }
